49066e1ed28a5a7e2329ca977ee79fb69e92d74d
Paulocereal <152792429+Paulocereal@users.noreply.github.com>
2026-02-08 16:27:23 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nr12: resolve(__dirname, 'nr-12.html'),
+        // Quando criar o PMOC, adicione a linha abaixo:
+        // pmoc: resolve(__dirname, 'pmoc.html'),
+      },
+    },
   },
 });
